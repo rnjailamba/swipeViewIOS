50bd01f9e3b463602690579e14e9462aebbf8dc4
rnjai <rnjailamba12@gmail.com>
2016-08-02 08:19:55 +0530

    first commit

---
+++ swipeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  swipeView
+//
+//  Created by Mr Ruby on 02/08/16.
+//  Copyright © 2016 Rnjai Lamba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ swipeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  swipeView
+//
+//  Created by Mr Ruby on 02/08/16.
+//  Copyright © 2016 Rnjai Lamba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
